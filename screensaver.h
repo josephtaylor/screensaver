a893d87c997837b82d5504566e654190f2d0b003
J Taylor OConnor <jtomusic@gmail.com>
2019-06-29 13:48:35 -0500

    comments

--- screensaver.h
+++ screensaver.h
@@ -13,8 +13,31 @@
 
 #define NCOLORS 32
 
+/*!
+ *
+ * This populates the given color with the r,g,b values.
+ *
+ * @param r         the red value
+ * @param g         the green value
+ * @param b         the blue value
+ * @param color     the color to be populated
+ */
 void color_rgb(unsigned short r, unsigned short g, unsigned short b, XColor *color);
+
+/*!
+ * Allocates a random set of colors that is NCOLORS long.
+ *
+ * @param display   the display
+ * @param colors    the color array to be populated
+ */
 void allocate_colors(Display *display, XColor colors[]);
+
+/*!
+ * This maps a value from -255 to 255 to USHORT_MIN to USHORT_MAX
+ *
+ * @param value     number between -255 and 255
+ * @return          the mapped number.
+ */
 unsigned short map_rgb(unsigned short value);
 
 #endif //SIMPLE_EXAMPLE_SCREENSAVER_H
